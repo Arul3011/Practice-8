ee2ac4582bb1ce423c5a9123e1899019159dba0d
Arul3011 <yarul8406@gmail.com>
2024-05-01 21:40:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import { FaBeer } from "react-icons/fa";
+
+function App() {
+  const [count, setCount] = useState(true)
+  const [num, setNum] = useState("")
+  const [court, setCourt] = useState("")
+  
+  useEffect(()=>{
+    const URL ="https://api.adviceslip.com/advice";
+    const fetchData= async()=>{ 
+    try{
+      const response = await fetch(URL);
+      const data = await response.json();
+      // console.log(data.slip.advice)
+      setNum(data.slip.id)
+      setCourt(data.slip.advice)
+    }catch(err){
+      console.log(err.message)
+    }
+  }
+  fetchData()
+  },[count])
+  // console.log(count);
+  return (
+    <>
+    <div className="cointainer">
+      <div className="cort">
+     <center>
+        <p className='advnum'>ADVICE#{num}</p>
+        <p className='adv'>"{court}"</p>
+        <div className='svg'></div>
+        <button className='span' onClick={()=>setCount(!count)}><FaBeer /></button>
+        </center>
+      
+      </div>
+      
+    </div>
+    </>
+  )
+}
+
+export default App
